d9ffb4db0b522548ebb7a5d0787c752076a01b3d
Rafekpro <codethegloobe@gmail.com>
2023-12-03 18:18:49 +0100

    initial springboot

---
+++ kavvka/src/main/java/com/example/kavvka/KavvkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kavvka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavvkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavvkaApplication.class, args);
+	}
+
+}
---
+++ kavvka/src/test/java/com/example/kavvka/KavvkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kavvka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavvkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
