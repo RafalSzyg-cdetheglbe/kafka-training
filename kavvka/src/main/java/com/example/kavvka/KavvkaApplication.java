3b8a5c6598bb10d790b6765552bcbba3d4e9b5e5
I583075 <rafal.szygula@sap.com>
2023-12-10 12:52:31 +0100

    structure

--- kavvka/src/main/java/com/example/kavvka/KavvkaApplication.java
+++ kavvka/src/main/java/com/example/kavvka/KavvkaApplication.java
@@ -3,10 +3,10 @@ package com.example.kavvka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class KavvkaApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(KavvkaApplication.class, args);
 	}
 
