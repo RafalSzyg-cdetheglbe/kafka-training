a07dbae4417f16918c5e5614c42c0ed7f865bac9
I583075 <rafal.szygula@sap.com>
2023-12-14 09:02:57 +0100

    more sketchy logic

--- kavvka/src/main/java/com/example/kavvka/producer/KafkaEventProducer.java
+++ kavvka/src/main/java/com/example/kavvka/producer/KafkaEventProducer.java
@@ -4,18 +4,22 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 
 public class KafkaEventProducer
 {
+	@Value("${kafka.topic}")
+	private String kafkaTopic;
+
 	JSONPObject jsonpObject;
 	private final KafkaProducer<String, Object> kafkaProducer;
 
 	public KafkaEventProducer(final Map<String, Object> configs)
 	{
 		this.kafkaProducer = new KafkaProducer(configs);
-		kafkaProducer.send(new ProducerRecord<>("test", jsonpObject.toString()));
+		kafkaProducer.send(new ProducerRecord<>(kafkaTopic, jsonpObject.toString()));
 	}
 
 }
